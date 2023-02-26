ec8b02c79f192118574cfc4f56d87c5a6330b135
durngyn <darrennguyen321919@gmail.com>
2023-02-25 16:23:27 -0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>Hi</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,9 @@
+import { NavLink } from 'react-router-dom'
+
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
