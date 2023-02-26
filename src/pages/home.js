b0cfe0d853718f1ba688c69ea112f64a95505bd5
durngyn <darrennguyen321919@gmail.com>
2023-02-25 20:05:31 -0800

    routing

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './../styles/Home.css'
+import { Link } from 'react-router-dom'
 export const Home = () => {
   return (
     <div>
@@ -36,7 +37,7 @@ export const Home = () => {
                 </div>
             </div>
             <div class='home-start-btn-div'>
-                <button>Start Game!</button>
+                <button><Link to="/game">Start Game!</Link></button>
             </div>
         </div>
         <div class='about us'>
