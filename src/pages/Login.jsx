0ce25d73a8a460c408c90c7c1cdc17eb44b92e2a
durngyn <darrennguyen321919@gmail.com>
2023-02-25 22:30:56 -0800

    button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,8 +11,10 @@ export const login = () => {
             <Name />
         </div>
         <div class='login-btn-div'>
-        <img class='spot'src='https://www.freepnglogos.com/uploads/spotify-logo-png/file-spotify-logo-png-4.png'/>
-            <button class='login-btn'><Link to="/home">Login with Spotify</Link></button>
+            <button class='login-btn'>
+            <img class='spot'src='https://www.freepnglogos.com/uploads/spotify-logo-png/file-spotify-logo-png-4.png'/>
+              <Link to="/home">Login with Spotify</Link>
+            </button>
             
         </div>
         <p class='login-btn-caption'>This will redirect you to the Spotify Auth Page</p>
