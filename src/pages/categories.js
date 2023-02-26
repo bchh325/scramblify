54a74f674c84ce21247301c629bf1fd11995ca57
kzmbo <alexkem132@gmail.com>
2023-02-25 17:00:44 -0800

    created category page

---
+++ src/pages/categories.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const categories = () => {
+  return (
+    <div class='categories-div'>
+        <div class='playlist-menu-div'>
+            <div class='playlist-img'>
+                <img alt='playlist-img'></img>
+            </div>
+            <div class='playlist-menu'>
+
+            </div>
+        </div>
+        <div class='categories-selection-div'>
+            <div class='artist-name-category'>
+
+            </div>
+            <div class='song-name-category'>
+
+            </div>
+            <div class='album-name-category'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default categories
