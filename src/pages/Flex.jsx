a4139a165d40c0c3ba8a8941d1350a76665b3b43
durngyn <darrennguyen321919@gmail.com>
2023-02-26 06:22:24 -0800

    hi

--- src/pages/Flex.jsx
+++ src/pages/Flex.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import './../styles/Flex.css'
 export default function Flex() {
   return (
-    <div>Flex</div>
+    <div class ="grid">
+        <div class="a">hi</div>
+        <div class="ab">
+            <div class="b">yo</div>
+            <div class="b">yo</div>
+        </div>
+
+    </div>
   )
 }
