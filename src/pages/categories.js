57005c708491ac0e13d9e47ed19e55c5e72a229d
kzmbo <alexkem132@gmail.com>
2023-02-25 20:50:43 -0800

    added category selector

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,23 +1,25 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../context/UserContext'
+import { CategoryContext } from '../context/CategoryContext'
 import { useEffect } from 'react'
 
 const categories = () => {
   const { user, setUser } = useContext(UserContext)
+  const { category, setCategory } = useContext(CategoryContext)
 
   const listOfPlaylist = [];
-  
+
   //Gets User's username and profile picture
   const GetUserPlaylist = async (token) => {
     await axios.get(`https://api.spotify.com/users/${user.user_id}/playlists`, {
-      headers: {Authorization: `Bearer ${token}`}
+      headers: { Authorization: `Bearer ${token}` }
     })
-    .then((response) => {
-      listOfPlaylist.push(response.body.items)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+      .then((response) => {
+        listOfPlaylist.push(response.body.items)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   useEffect(() => {
@@ -25,28 +27,31 @@ const categories = () => {
   })
 
   return (
-    <div class='categories-div'>
+    <UserContext.Provider value={{ category, setCategory }}>
+      <div class='categories-div'>
         <div class='playlist-menu-div'>
-            <div class='playlist-menu'>
-              {
-                listOfPlaylist.map((elem) => {
-                  return <li>{elem.name}</li>
-                })
-              }
-            </div>
+          <div class='playlist-menu'>
+            {
+              listOfPlaylist.map((elem) => {
+                return <li>{elem.name}</li>
+              })
+            }
+          </div>
         </div>
         <div class='categories-selection-div'>
-            <div class='artist-name-category'>
-
-            </div>
-            <div class='song-name-category'>
-
-            </div>
-            <div class='album-name-category'>
-
-            </div>
+          <div class='artist-name-category'>
+              <button onClick={setCategory(() => {category = "artist_name"})}>test</button>
+          </div>
+          <div class='song-name-category'>
+            <button onClick={setCategory(() => {category = "artist_name"})}>test</button>
+          </div>
+          <div class='album-name-category'>
+            <button onClick={setCategory(() => {category = "artist_name"})}>test</button>
+          </div>
         </div>
-    </div>
+      </div>
+    </UserContext.Provider>
+
   )
 }
 
