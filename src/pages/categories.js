60d0e4702fc22e29eb3790101f04123350ec3de8
kzmbo <alexkem132@gmail.com>
2023-02-25 19:56:27 -0800

    able to show user's playlist

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
+import { useEffect } from 'react'
 
 const categories = () => {
+  const { user, setUser } = useContext(UserContext)
+
+  const listOfPlaylist = [];
+  
+  //Gets User's username and profile picture
+  const GetUserPlaylist = async (token) => {
+    await axios.get(`https://api.spotify.com/users/${user.user_id}/playlists`, {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then((response) => {
+      listOfPlaylist.push(response.body.items)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    GetUserPlaylist(user.token)
+  })
+
   return (
     <div class='categories-div'>
         <div class='playlist-menu-div'>
-            <div class='playlist-img'>
-                <img alt='playlist-img'></img>
-            </div>
             <div class='playlist-menu'>
-
+              {
+                listOfPlaylist.map((elem) => {
+                  return <li>{elem.name}</li>
+                })
+              }
             </div>
         </div>
         <div class='categories-selection-div'>
