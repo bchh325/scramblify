144632fd5709ffb97487bd3d8d3e98e7654eac3d
kzmbo <alexkem132@gmail.com>
2023-02-25 17:23:12 -0800

    created game pg

---
+++ src/pages/game.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const game = () => {
+  return (
+    <div class='game-pg'>
+        <div class='score-div'>
+            <h1>Score: <p>{'score'}</p></h1>
+        </div>
+        <div class='song-img-div'>
+            <img alt='current song img'></img>
+        </div>
+        <div class='scrambled-word-div'>
+            <p>{scrambledTitle}</p>
+        </div>
+        <div class='fill-in-text-div'>
+            <input type='text'></input>
+        </div>
+        <div class='submit-hint-btn-div'>
+            <button class='submit-btn'>Submit!</button>
+            <button class='hint-btn'>Hint?</button>
+        </div>
+    </div>
+  )
+}
+
+export default game
