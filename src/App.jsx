174130ff043bfbffc6be465db72d72191eb657a0
kzmbo <alexkem132@gmail.com>
2023-02-25 19:56:45 -0800

    added vars and context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import Name from './title/Name';
 import './App.css';
+import { UserContext } from './context/UserContext';
+import { useState } from 'react';
+import axios from 'axios';
+
+
 
 function App() {
+  const client_id = "d559adf71389493dbd4b84821189173a";
+  const response_type = 'token';
+  const client_secret = "c0cf5ad3734e4e249295db892869ecd5";
+  const redirect_uri = "http://localhost:3000/";
+  const auth_URI = 'http://accounts.spotify.com/en/authorize'
+
+  const {userObj, setUserObj} = useState({})
+
+
   return (
     <div className="App">
-      <Name />
+      <UserContext.Provider value={{ userObj, setUserObj}}>
+        <Name />
+      </UserContext.Provider>
     </div>
   );
 }
