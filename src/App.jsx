492ae6ab32d7e235fed9322c5ef2c2c19961dc33
durngyn <darrennguyen321919@gmail.com>
2023-02-25 19:32:43 -0800

    body fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import Name from './title/Name';
 import './App.css';
+
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Name />
+      <Routes>
+        <Route path="/" element={<Name />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./../styles/loginPage.css" 
 
-const login = () => {
+export const login = () => {
   return (
     <div class='login-pg'>
         <div class='login-title-div'>
